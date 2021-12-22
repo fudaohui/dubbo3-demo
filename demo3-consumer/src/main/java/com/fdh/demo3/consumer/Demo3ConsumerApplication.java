9a4ce674b469a03432a7dca1fab658cfe29705eb
daemon <deimon@126.com>
2021-12-22 22:08:31 +0800

    triple+protobuf springboot tested it!

--- demo3-consumer/src/main/java/com/fdh/demo3/consumer/Demo3ConsumerApplication.java
+++ demo3-consumer/src/main/java/com/fdh/demo3/consumer/Demo3ConsumerApplication.java
@@ -1,6 +1,9 @@
 package com.fdh.demo3.consumer;
 
 import com.fdh.dubbo3.service.DemoService;
+import com.fdh.dubbo3.service.GreeterService;
+import com.fdh.dubbo3.service.HelloReply;
+import com.fdh.dubbo3.service.HelloRequest;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
@@ -16,19 +19,22 @@ import java.util.concurrent.CountDownLatch;
 public class Demo3ConsumerApplication {
 
     @DubboReference
-    private DemoService demoService;
+    private GreeterService greeterService;
 
     public static void main(String[] args) throws InterruptedException {
 
         ConfigurableApplicationContext context = SpringApplication.run(Demo3ConsumerApplication.class, args);
         Demo3ConsumerApplication application = context.getBean(Demo3ConsumerApplication.class);
-        String result = application.doSayHello("world");
+        String result = application.doSayHello();
         System.out.println("*************result: " + result);
         new CountDownLatch(1).await();
     }
 
-    public String doSayHello(String name) {
-        return demoService.sayHello(name);
+    public String doSayHello() {
+        HelloReply helloReply = greeterService.sayHello(HelloRequest.newBuilder()
+                .setName("triple9999")
+                .build());
+        return helloReply.getMessage();
     }
 
 }
